1885eb12ee9400f7b5a02be8e94d85765a660d33
Lemuel Robinson <132776922+lrobin42@users.noreply.github.com>
2025-03-15 20:03:44 -0700

    Update password_generator.py
    editing formatting for readability

--- password_generator.py
+++ password_generator.py
@@ -2,13 +2,12 @@ import string
 from random import randint
 from typing import Literal, LiteralString
 
-def generate_random_password(
-    capital_letters=True, numbers=True, symbols=True, password_length=25) -> LiteralString:
+def generate_random_password(capital_letters=True, numbers=True, symbols=True, password_length=25) -> LiteralString:
     capital_chars: LiteralString = string.ascii_uppercase
     lowercase_chars: LiteralString = string.ascii_lowercase
     number_chars = "0123456789"
     symbol_chars = "!@#$%^&*()_+=-\\|]{}'\",<.?>;:"
-    password: Literal[""] = ""
+    password = ""
 
     password_characters: LiteralString = lowercase_chars
     if capital_letters:
